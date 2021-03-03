b1551295472f39bdfc8a7267efead17d675e26ba
AnonymousDapper <10648453+AnonymousDapper@users.noreply.github.com>
2021-03-03 14:43:32 -0500

    'final' version of db schema for now

--- cogs/utils/schema.sql
+++ cogs/utils/schema.sql
@@ -2,61 +2,83 @@
 --
 -- Copyright (c) 2021 Mieszko Exchange
 
--- TODO: get mysql setup first lol
-
 -- Currency data storage
--- TODO: maybe pull this directly from PaymentsDev.Currency instead?
-CREATE TABLE `Currency` (
-  `id` int(10) unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `code` varchar(4) NOT NULL UNIQUE KEY,
-  `precision` tinyint(3) unsigned NOT NULL
+CREATE TABLE Currency (
+  id int(10) unsigned NOT NULL AUTO_INCREMENT,
+  code varchar(4) NOT NULL UNIQUE KEY,
+  `precision` tinyint(3) unsigned NOT NULL,
+  PRIMARY KEY (id)
+  KEY (code),
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Database error logging
-CREATE TABLE `LoggedError` (
-    `pk` int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE LoggedError (
+    pk int(10) NOT NULL AUTO_INCREMENT,
     `level` enum('CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG') NOT NULL,
-    `module` tinytext NOT NULL,
-    `function` tinytext NOT NULL,
-    `filename` tinytext NOT NULL,
-    `line` smallint unsigned NOT NULL,
-    `message` varchar(1000) NOT NULL,
-    `timestamp` datetime NOT NULL
+    module tinytext NOT NULL,
+    function tinytext NOT NULL,
+    filename tinytext NOT NULL,
+    lineno smallint unsigned NOT NULL,
+    message varchar(1000) NOT NULL,
+    `timestamp` datetime NOT NULL,
+    PRIMARY KEY (pk)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Discord <-> Bot connection
-CREATE TABLE `User` (
-    `discordId` bigint unsigned NOT NULL PRIMARY KEY,
-    `createdAt` datetime NOT NULL,
-    `locked` tinyint(1) unsigned NOT NULL DEFAULT 0
+CREATE TABLE User (
+    discordID bigint unsigned NOT NULL,
+    createdAt datetime NOT NULL,
+    locked tinyint(1) unsigned NOT NULL DEFAULT 0,
+    defaultAddress varchar(256),
+    PRIMARY KEY (discordID)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-
 -- Addresses stored for convienence
-CREATE TABLE `LinkedAddress` (
-    `userId` bigint unsigned NOT NULL PRIMARY KEY,
-    `currency` int(10) unsigned NOT NULL,
-    `address` tinytext NOT NULL,
-    `locked` tinyint(1) unsigned NOT NULL DEFAULT 0,
-    KEY `currency` (`currency`),
-    FOREIGN KEY (`userID`) REFERENCES `User` (`discordId`) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (`currency`) REFERENCES `Currency` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
+CREATE TABLE LinkedAddress (
+    userID bigint unsigned NOT NULL,
+    currency int(10) unsigned NOT NULL,
+    address varchar(256) NOT NULL,
+    locked tinyint(1) unsigned NOT NULL DEFAULT 0,
+    PRIMARY KEY (userID, address),
+    KEY (currency),
+    FOREIGN KEY (userID) REFERENCES User (discordID) ON UPDATE CASCADE,
+    FOREIGN KEY (currency) REFERENCES Currency (id) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Escrow stuff
-CREATE TABLE `EscrowPayment` (
-    `id` serial PRIMARY KEY,
-    `currency` int(10) unsigned NOT NULL,
-    `sender` bigint unsigned NOT NULL,
-    `receiver` bigint unsigned NOT NULL,
+CREATE TABLE EscrowPayment (
+    id serial, -- necessary for EscrowEvent relation
+    currency int(10) unsigned NOT NULL,
+    sender bigint unsigned NOT NULL,
+    receiver bigint unsigned NOT NULL,
+    sourceAddress varchar(256) NOT NULL,
+    destAddress varchar(256) NOT NULL,
+    status enum('pending', 'paid', 'complete') NOT NULL,
+    amount decimal(24, 12) unsigned NOT NULL,
+    startedAt timestamp NOT NULL,
+    lastActionAt timestamp,
+    PRIMARY KEY (sender, receiver),
+    KEY (sourceAddress, destAddress),
+    KEY (id),
+    KEY (currency),
+    KEY (sender),
+    KEY (receiver),
+    FOREIGN KEY (currency) REFERENCES Currency (id) ON UPDATE CASCADE,
+    FOREIGN KEY (sender) REFERENCES User (discordID) ON UPDATE CASCADE,
+    FOREIGN KEY (receiver) REFERENCES User (discordID) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-
-    PRIMARY KEY (`sender`, `reciever`),
-    KEY `currency` (`currency`),
-    KEY `sender` (`sender`),
-    KEY `receiver` (`receiver`),
-
-    FOREIGN KEY (`curency`) REFERENCES `Currency` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (`sender`) REFERENCES `User` (`discordId`) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (`receiver`) REFERENCES `User` (`discordId`) ON UPDATE CASCADE ON DELETE CASCADE,
+-- Escrow action details
+CREATE TABLE EscrowEvent (
+    paymentID serial,
+    action enum('cancel', 'release'),
+    actioner enum('sender', 'receiver', 'moderator') NOT NULL,
+    actionerID bigint unsigned NOT NULL,
+    actionAt timestamp NOT NULL,
+    actionMsg tinytext,
+    PRIMARY KEY (paymentID),
+    KEY (actioner),
+    KEY (actionerID),
+    FOREIGN KEY (paymentID) REFERENCES EscrowPayment (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (actionerID) REFERENCES User (discordID) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
